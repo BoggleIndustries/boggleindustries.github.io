85a557a6fada7075a7560297e3a9eb2683ba61c4
BoggleIndustries <BoggleIndustries@gmail.com>
2016-06-25 18:54:36 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta charset="utf8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>2nd EU referendum breakdown</h1>
+        <p>Just who exactly is making this petition grow large?</p>
+        <p>Well looking at the data only <span id="num"></span> of signees are from Great Britain.</p>
+        <small> Data source: <a href="https://petition.parliament.uk/petitions/131215.json">https://petition.parliament.uk/petitions/131215.json</a></small>
+    
+        <canvas id="pie" height="1000px"></canvas>
+
+        <script src="chart.js"></script>
+        <script src="jquery.js"></script>
+        <script src="data.js"></script>
+        <script src="app.js"></script>
+    </body>
+</html>
